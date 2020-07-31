59c9a9822ee880c85b33277cbeda2219599b46e8
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-07-30 20:25:45 -0400

    CommentDetail component created and exported and imported to index.js

--- components/src/index.js
+++ components/src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail'
 
 const App = () => {
     return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}></img>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00 PM</span>
-                    </div>
-                    <div className="text">Nice Blog ma dude</div>
-                </div>
-            </div>
-        </div>
+        <CommentDetail />
     )
 };
 
