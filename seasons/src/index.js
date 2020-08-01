66c950dcbcc39589a7eb6d032378dd12c40d9fd2
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-08-01 16:01:24 -0400

    window geolocator

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position), 
+    (err) => console.log(err)
+  )
   return <div>Hi there</div>
 };
 
