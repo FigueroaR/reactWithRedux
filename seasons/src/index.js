3a7e514809235ce2ee21292314aaf9c8174fea6a
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-08-01 17:31:15 -0400

    class constructor with super props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-
-
 class App extends React.Component {
+  constructor(props) {
+    super(props) // super is always used with pur contructor or error
 
+    this.state = {lat: null}
+  }
+
+
+  // React sasys we have to define Render!!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position), 
       (err) => console.log(err)
-    )
+    );
+
     return <div>Latitude</div>
   }
 }
