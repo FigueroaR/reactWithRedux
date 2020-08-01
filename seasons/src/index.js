88f9f800ef7e6056e4e1b4e40c83565976553774
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-08-01 17:45:27 -0400

    updating state properties

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(props) {
-    super(props) // super is always used with pur contructor or error
+    super(props) // super is always used with our contructor or error
 
+    //this the only time we do direct assignment to this.setState
     this.state = {lat: null}
   }
 
@@ -12,11 +13,14 @@ class App extends React.Component {
   // React sasys we have to define Render!!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position), 
+      position => {
+        // we called setState function
+        this.setState({ lat: position.coords.latitude})
+      }, 
       (err) => console.log(err)
     );
 
-    return <div>Latitude</div>
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
