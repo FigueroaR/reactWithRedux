89721b3f4ca52aee304cc8b9b015a19661fcd3a5
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-08-01 18:46:27 -0400

    graceful error

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,21 +6,32 @@ class App extends React.Component {
     super(props) // super is always used with our contructor or error
 
     //this the only time we do direct assignment to this.setState
-    this.state = {lat: null}
-  }
+    this.state = {lat: null, errorMessage: ''};
 
-
-  // React sasys we have to define Render!!!
-  render() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         // we called setState function
         this.setState({ lat: position.coords.latitude})
       }, 
-      (err) => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message})
+      }
     );
+  }
 
-    return <div>Latitude: {this.state.lat}</div>
+  // React sasys we have to define Render!!!
+  render() {
+
+    if (this.state.errorMessage && !this.state.lat) {
+    return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>...Loading!...</div>
+    
   }
 }
 
