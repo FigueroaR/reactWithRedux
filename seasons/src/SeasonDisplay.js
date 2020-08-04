ecff5b61ee97cec8608bab60c7f3bbcad25640bb
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-08-04 17:35:15 -0400

    descontructed information displaying

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
-const getSeason = (lat, month) => {
-    if (month > 2 && month < 9 ) {
-        return lat > 0 ? 'Summer' : 'Winter'
-    } else {
-        return lat > 0 ? 'Winter' : 'Summer'
+const seasonConfig = {
+    summer: {
+        text: 'Swiming with fish!',
+        iconName: 'summer'
+    },
+    winter: {
+        text: 'Burr its cold!',
+        iconName: 'winter'
     }
 }
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9 ) {
+        return lat > 0 ? 'summer' : 'winter'
+    } else {
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
 const SeasonDisplay = (props) => {
-    console.log(props, "---------", getSeason())
+    //console.log(props, "---------", getSeason())
     const season = getSeason(props.latitude, new Date().getMonth())
-    const text = season === 'winter' ? 'Burrr! it cold! uhh' : "Swiming with fish"
-    const icon = season === 'winter' ? 'snowflake' : "sun"
+    console.log(season)
+    const { text, iconName} = seasonConfig[season];
+
     return (
     <div>
-            <i className={`${icon} icon`} />
+            <i className={`${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${icon} icon`} />
+            <i className={`${iconName} icon`} />
         </div>)
 }
 
