fd90c404f7663b657b46e2043dded747d3849673
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-08-04 13:59:27 -0400

    seasons display with logic

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9 ) {
+        return lat > 0 ? 'Summer' : 'Winter'
+    } else {
+        return lat > 0 ? 'Winter' : 'Summer'
+    }
+}
+const SeasonDisplay = (props) => {
+    console.log(props, "---------", getSeason())
+    const season = getSeason(props.latitude, new Date().getMonth())
     return (
-        <div>Season Display</div>
+        <div>Season Display : {season} </div>
     )
 }
 
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props) // super is always used with our contructor or error
 
-    //this the only time we do direct assignment to this.setState
-    this.state = {lat: null, errorMessage: ''};
+  state = {lat: null, errorMessage: ''};
 
+  componentDidMount() {
+    console.log("DID MOUNT")
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // we called setState function
-        this.setState({ lat: position.coords.latitude})
-      }, 
-      err => {
-        this.setState({ errorMessage: err.message})
-      }
+      position =>  this.setState({ lat: position.coords.latitude}), 
+      err => this.setState({ errorMessage: err.message})
     );
+  }
+
+  componentDidUpdate() {
+    console.log("DID UPDATE")
   }
 
   // React sasys we have to define Render!!!
@@ -27,7 +26,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay latitude={this.state.lat}/>
     }
 
     return <div>...Loading!...</div>
