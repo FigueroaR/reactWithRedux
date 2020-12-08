99d74e8e66b9e073347f542ee2f45877e69223e0
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-08 18:06:51 -0500

    pics: ImagList with CSS

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import './ImageList.css';
+import React from 'react';
 
 const ImageList = (props) => {
     //                 The images that would be KEY is being deconstructed
     const img = props.images.map( ({id, description, urls}) => {
         return <img src={urls.regular} key={id} alt={description}/>
     });
-return <div>{img}</div>
+return <div className="image-list">{img}</div>
 }
 
 
