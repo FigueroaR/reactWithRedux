0a1942c100e3a3d4fe3b7730a1d5a32dc77913f8
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-08 18:24:01 -0500

    pics: accesing the DOM with refs

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,11 @@
 import './ImageList.css';
+import ImageCard from './ImageCard';
 import React from 'react';
 
 const ImageList = (props) => {
     //                 The images that would be KEY is being deconstructed
-    const img = props.images.map( ({id, description, urls}) => {
-        return <img src={urls.regular} key={id} alt={description}/>
+    const img = props.images.map( (image) => {
+        return <ImageCard  key={image.id} image={image}/>
     });
 return <div className="image-list">{img}</div>
 }
