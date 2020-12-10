1111324ea0a5c425aa67d1d2c198170afdaa2d91
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-10 16:53:30 -0500

    new app :videos:

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -4,28 +4,35 @@ import React from 'react';
 class ImageCard extends React.Component {
     constructor() {
         super();
+        this.state = {spans: 0};
+
+        //  we create a reference we can interact with on the DOM, like a tag
         this.imageRef = React.createRef();
     }
 
     // this our way of grabing the image tag after it shows on DOM as HTML
     componentDidMount() {
-        console.log('comp did mount', this.imageRef.current.clientHeight)
         // error avoiding by listening only for twhen the image loads
         this.imageRef.current.addEventListener('load', this.setSpans)
     }
 
+    // we are setting the spans, 
+    // these will be used to know how much to expand the pictures
     setSpans = () => {
-        console.log('spans', this.imageRef.current.clientHeight)
+        const height = this.imageRef.current.clientHeight
+        const spans = Math.ceil(height / 10 );
 
-
+        this.setState({spans});
     };
 
     render () {
 
         // destructuring
         const {description, urls } = this.props.image
+
+        // we added a style to the div containing the pic
         return (
-            <div>
+            <div style={{gridRowEnd: `span ${this.state.spans}` }}>
                 <img ref={this.imageRef} alt={description} src={urls.regular}/>
             </div>
         )
