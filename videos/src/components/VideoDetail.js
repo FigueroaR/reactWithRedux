5caebb82db910e09c593f2108fe46dac670cebdb
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-19 00:47:36 -0500

    videos: fixing a few warnings

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import VideoList from './VideoList';
+
 
 const VideoDetail = ({video}) => {
     if (!video) {
@@ -11,7 +11,7 @@ const VideoDetail = ({video}) => {
     return (
         <div>
             <div className="ui embed">
-                <iframe src={videoSrc} />
+                <iframe title='video player' src={videoSrc} />
             </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './VideoItem.css';
-import VideoList from './VideoList';
 
-const Videoitem = ({ video, onVideoSelect })=> {
+
+const Videoitem = ({ video, onVideoSelect , id})=> {
+    
     return (
         <div className="video-item item" onClick={ () => onVideoSelect(video)}>
-            <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+            <img id={id}  alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url}/>
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
                 <div className="description">{video.snippet.description}</div>
