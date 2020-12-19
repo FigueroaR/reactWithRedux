2c4ff55be47a8bb60faa038a951227db237433c6
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-18 23:06:08 -0500

    styling a list

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './VideoItem.css';
+import VideoList from './VideoList';
 
-const Videoitem = props => {
-    return <div>Video Item</div>
+const Videoitem = ({ video })=> {
+    return (
+        <div className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+                <div className="description">{video.snippet.description}</div>
+            </div>
+            
+        </div>
+        )
 }
 
 
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,11 +2,11 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({videos}) => {
-    const renderedList = videos.map( (videos) => {
-        return <VideoItem  />
+    const renderedList = videos.map( (video) => {
+        return <VideoItem video={video} />
     })
 
-    return <div>{renderedList} </div>
+    return <div className="ui relax divided list">{renderedList} </div>
 }
 
 
