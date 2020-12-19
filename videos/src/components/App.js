e895dc2298fcf52c01cfee170a56838bdbc9e175
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-19 00:02:47 -0500

    Videos: styling the VideoDetail

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 
 class App extends React.Component {
 
-    state = {videos: []}
+    state = {videos: [], selectedVideo: null}
 
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -19,13 +20,14 @@ class App extends React.Component {
     }
 
     onVideoSelect = (video) => {
-        console.log('video from app', video)
+        this.setState({ selectedVideo: video })
     }
 
     render() {
         return (
         <div className="ui container"> 
             <SearchBar onTermSubmit={this.onTermSubmit} />
+            <VideoDetail video={this.state.selectedVideo} />
             <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
         </div>
         )
