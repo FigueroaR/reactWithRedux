85a75250b83b7fbc3a29fd5ce618ba0c75297e0b
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-20 16:28:04 -0500

    widgets: Communicating the item props

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front and JS framework'
+    },
+    {  
+        title: 'Why use React?',
+        content: 'React is a favoorite JS library'
+    },
+    {
+        title: 'How to use react?',
+        content: 'By creating components'
+    }
+];
 
 export default () => {
-    return <h1>widget</h1>;
+    return (
+        <div>
+            <Accordion items={items}/>
+        </div>
+    ) 
 }
