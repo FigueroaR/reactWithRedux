3bec08394eae0518ef18ab101b12d0a747e98a31
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-28 17:34:53 -0500

    widgets: Adding useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
 const [term, setTerm] = useState('')
+
+// we configure useEffect to be used on first render, rerender, or change
+// we can add a second argument like array
+
+// array : initial render 
+// nothing : initial render and rerender 
+// array with data : initial R, rerender , and rerender if data change 
+useEffect(() => {
+    console.log("logging effect")
+}, [term])
 
     return (
         <div>
