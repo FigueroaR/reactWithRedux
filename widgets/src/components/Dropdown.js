50e70eefd9d03089204f00e62eb9d3ec32eb7b81
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-30 17:59:19 -0500

    widgets: binding an event listener

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,41 +1,47 @@
-import react, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-    const [open, setopen] = useState(false)
+  const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', () => {
+        setOpen(false);
+        }, { capture: true });
+    }, []);
 
     const renderedOptions = options.map((option) => {
-
         if (option.value === selected.value) {
-            return null
+        return null;
         }
 
         return (
-            <div 
-                key={option.value} 
-                onClick={() => onSelectedChange(option) }
-                className="item">
-                    {option.label}
-            </div>
-      );
+        <div
+            key={option.value}
+            className="item"
+            onClick={() => onSelectedChange(option)}
+        >
+            {option.label}
+        </div>
+        );
     });
 
-    return (
-        <div className='ui form'>
-            <div className='field'>
-                <label className='label'>Select a color</label>
-                <div 
-                onClick={() => setopen(!open)} 
-                className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
-                    <i className='downdown icon'></i>
-                    <div className='text'>{selected.label}</div>
-                    <div className={`menu ${open ? "visible transition" : ""}`}>
-                        {renderedOptions}
-                    </div>
-                </div>
+  return (
+    <div className="ui form">
+        <div className="field">
+            <label className="label">Select a Color</label>
+            <div
+            onClick={() => setOpen(!open)}
+            className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+            >
+            <i className="dropdown icon"></i>
+            <div className="text">{selected.label}</div>
+            <div className={`menu ${open ? 'visible transition' : ''}`}>
+                {renderedOptions}
+            </div>
             </div>
         </div>
-    )
-}
-
+    </div>
+    );
+};
 
 export default Dropdown;
