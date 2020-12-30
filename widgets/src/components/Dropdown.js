356cdc7e6bf8928fbfe8f0dd62157aff611adfc3
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2020-12-30 15:46:15 -0500

    widgtes: lots of JSX

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,28 @@
 import react from 'react';
 
-const Dropdown = () => {
-    return <h1>Drop</h1>
+const Dropdown = ({ options }) => {
+    const renderedOptions = options.map((option) => {
+      return (
+        <div key={option.value} className="item">
+          {option.label}
+        </div>
+      );
+    });
+
+    return (
+        <div className='ui form'>
+            <div className='field'>
+                <label className='label'>Select a color</label>
+                <div className='ui selection dropdown visible active'>
+                    <i className='downdown icon'></i>
+                    <div className='text'>Select a color</div>
+                    <div className='menu visible transition'>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 
