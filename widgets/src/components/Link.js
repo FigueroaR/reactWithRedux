ab2437315589ffe41fc3ddf19aa26e2a41ebb060
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-08 15:42:20 -0500

    Handling Command Clicks

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const Link =({href, className, children}) => {
     const handleClick = (e) => {
+        if (e.metaKey || e.ctrlKey) {
+            // when we press ctrl and click on option link
+            // if either is true, we return early
+            return
+        }
+
+
         e.preventDefault();
         window.history.pushState({}, '', href)
-        
+
         const navEvent = new PopStateEvent('popstate');
+        //dispatchEvent is built in
         window.dispatchEvent(navEvent);
     }
 
