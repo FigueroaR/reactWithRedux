a714c4169ef14bbbdca596217adaa7aa07ba1e53
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-08 14:59:47 -0500

    changing the URL

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Link =({href, className, children}) => {
     const handleClick = (e) => {
         e.preventDefault();
+        window.history.pushState({}, '', href)
+        
     }
 
     return <a 
