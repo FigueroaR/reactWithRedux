dc320340e3be98ca0d3fde296d69acfc783d6b3c
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-13 20:08:44 -0500

    building reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducers = () => {
+    return [
+        {title: 'No Scrubs', durantion: '4:05'},
+        {title: 'Macarena', durantion: '2:30'},
+        {title: 'All Star', durantion: '3:15'},
+        {title: 'I Want It That Way', durantion: '1:45'}
+    ]
+}
+
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong
+}
