848ff8bd426dcd4845cc80597ff0681bd162ca2c
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-14 19:04:19 -0500

    calling Actions Creators from Components

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import { connect } from './react-redux';
- 
-class _SongList extends React.Component {
+import React from 'react'
+import {connect} from 'react-redux';
+import { selectSong } from '../actions'
+
+class SongList extends React.Component {
+    
     renderList() {
-        return this.props.songs.map(song => {
+        return this.props.songs.map((song) => {
             return (
-                <div className="item" key={song.title}>
-                    <div className="right floated content">
-                        <div className="ui button primary">Select</div>
+                <div className='item' key={song.title}>
+                    <div className='right floated content'>
+                        <button 
+                            className='ui button primary'
+                            onClick={() => this.props.selectSong(song)}
+                        >Select</button>
                     </div>
-                    <div className="content">
-                        {song.title}
-                        <div>
-                            <b>{song.title === this.props.favoriteTitle && 'Favorite!'}</b>
-                        </div>
-                    </div>
+                    <div className='content'>{song.title}</div>
                 </div>
-            );
-        });
+            )
+        })
     }
- 
+
     render() {
-        return <div className="ui divided list">{this.renderList()}</div>;
+        
+        return (<div className="ui divided list">{this.renderList()}</div>)
     }
 }
- 
+
 const mapStateToProps = state => {
-    return { songs: state.songs, favoriteTitle: state.favoriteTitle };
-};
- 
-export const SongList = connect(mapStateToProps)(_SongList);
+    //console.log(state)
+    return {songs: state.songs}
+}
+
+export default connect(mapStateToProps, {selectSong})(SongList);
+
