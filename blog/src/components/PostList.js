b7d0d94a4fdc646663df34dde365139954c51b9e
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-23 19:38:43 -0500

    wiring up an Action Creator

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { fetchPosts } from '../actions'
+import {connect} from 'react-redux'
 
 class PostList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render () {
         return <div>Post List</div>
     }
 }
 
-
-export default PostList;
+// mapStatetoProps ism not used so null 
+export default  connect(null, {fetchPosts})(PostList);
