a5cb964d40d8a687922d786aceecaf94c9564d3e
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-23 21:35:55 -0500

    shortened syntax with redux thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,12 +2,10 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 // we use the pre configured axios instance 
 //to make a request in our actions creator
 
-export const fetchPosts = () => {
+export const fetchPosts = () => async (dispatch) => {
     // we access axios with a .get request
-    async dispatch => {
-        const promise = await jsonPlaceholder.get('/Posts')
-        dispatch({type: 'FETCH_POSTS', payload: promise}) 
-    }
+        const response = await jsonPlaceholder.get('/Posts')
+        dispatch({type: 'FETCH_POSTS', payload: response})
     
 }
 
