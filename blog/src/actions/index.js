0af837e880bc3fe18c43f601c906c2d762d3207a
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-23 21:28:23 -0500

    shortened systanx with redux thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,15 +2,12 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 // we use the pre configured axios instance 
 //to make a request in our actions creator
 
-
-// we are making this function async
-export const fetchPosts = async () => {
+export const fetchPosts = () => {
     // we access axios with a .get request
-    const response = await jsonPlaceholder.get('./Posts')
-
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
+    async dispatch => {
+        const promise = await jsonPlaceholder.get('/Posts')
+        dispatch({type: 'FETCH_POSTS', payload: promise}) 
     }
+    
 }
 
