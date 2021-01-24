5c515483db7e4ea0fa48dfc19057aa6a77e06cb4
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-23 20:14:05 -0500

    Making a request from action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,16 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+// we use the pre configured axios instance 
+//to make a request in our actions creator
+
+
+// we are making this function async
+export const fetchPosts = async () => {
+    // we access axios with a .get request
+    const response = await jsonPlaceholder.get('./Posts')
+
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     }
 }
 
