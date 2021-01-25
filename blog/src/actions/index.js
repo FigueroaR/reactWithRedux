c28036590673e120fea765b1067213ca505c7585
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-25 15:27:41 -0500

    dispatch correct values

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,10 +2,11 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 // we use the pre configured axios instance 
 //to make a request in our actions creator
 
-export const fetchPosts = () => async (dispatch) => {
+export const fetchPosts = () => async dispatch => {
     // we access axios with a .get request
-        const response = await jsonPlaceholder.get('/Posts')
-        dispatch({type: 'FETCH_POSTS', payload: response})
+    const response = await jsonPlaceholder.get('/posts')
+
+    dispatch({type: 'FETCH_POSTS', payload: response.data})
     
 }
 
