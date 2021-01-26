db633cd0f93b15ecb22e27422b079dc8ee880778
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-26 18:42:21 -0500

    wiring up routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,23 +1,20 @@
 import React from 'react'
-import {BrowserRouter, Route, Link} from 'react-router-dom'
-
-const PageOne = () => {
-    return (<div>
-        <Link to='/pageTwo'>Go to page two!</Link>
-        </div>)
-}
-
-const PageTwo = () => {
-    return (<div>
-        <Link to='/'>Navigate to page one!</Link>
-    </div>)
-}
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate'
+import StreamList from './streams/StreamList'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamShow from './streams/StreamShow'
 
 const App = () => {
     return (<div>
         <BrowserRouter>
-        <div><Route path='/' exact component={PageOne}/>
-            <Route path='/pageTwo' component={PageTwo}/>
+        <div>
+            <Route path='/' exact component={StreamList}/>
+            <Route path='/streams/new' exact component={StreamCreate}/>
+            <Route path='/streams/edit' exact component={StreamEdit}/>
+            <Route path='/streams/delete' exact component={StreamDelete}/>
+            <Route path='/streams/show' exact component={StreamShow}/>
         </div>
         </BrowserRouter>
     </div>)
