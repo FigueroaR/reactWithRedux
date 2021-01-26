51ef5a5e4cf0b83a0719b238cca5938b6600dc73
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-01-26 18:00:08 -0500

    component scaffolding

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
 const PageOne = () => {
-    return <div>Page one</div>
+    return (<div>
+        <Link to='/pageTwo'>Go to page two!</Link>
+        </div>)
 }
 
 const PageTwo = () => {
-    return <div>Page two</div>
+    return (<div>
+        <Link to='/'>Navigate to page one!</Link>
+    </div>)
 }
 
 const App = () => {
     return (<div>
         <BrowserRouter>
-            <Route path='/' exact component={PageOne}/>
+        <div><Route path='/' exact component={PageOne}/>
             <Route path='/pageTwo' component={PageTwo}/>
+        </div>
         </BrowserRouter>
     </div>)
 }
