b84813e20698655a915376ff8689262798c61212
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-05 16:06:47 -0500

    creating forms

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,7 +1,10 @@
+
+import {reducer as formReducer} from 'redux-form'
 import { combineReducers } from 'redux';
 import authReducer from './authReducer'
 
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
