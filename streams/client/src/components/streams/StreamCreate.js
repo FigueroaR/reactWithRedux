b469e1a2a5f2986b2a98e97bd7f0ddf2f8fde704
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-05 18:55:55 -0500

    validation of Form Inputs

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Field, reduxForm} from 'redux-form';
+import {clearSubmitErrors, Field, formValues, reduxForm} from 'redux-form';
 
 class  StreamCreate extends React.Component {
 
@@ -12,14 +12,34 @@ class  StreamCreate extends React.Component {
         )
     }
 
+    onSubmit(formValues) {
+        console.log(formValues)
+    }
+
     render(){
         
         return (
-        <form className='ui form'>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
             <Field name="title" component={this.renderInput} label="Enter Title"/>
             <Field name="description" component={this.renderInput} label="Enter Description"/> 
+            <button className="ui button primary" >Submit</button>
+
         </form>)
     }
+}
+
+const validate = (formValues) => {
+    const errors = { }
+
+    if (!formValues.title) {
+        errors.title = 'Enter title!';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'Enter description!';
+    }
+
+    return errors;
 }
 
 
