3bc52068ce0f3a8a7d5318ccd85a033bf2f4dfd4
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-11 13:27:42 -0500

    deleting properties with Omit

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,5 +1,6 @@
 import {CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, EDIT_STREAM, DELETE_STREAM } from '../actions/types'
-
+import _ from 'loadash'
+//we import the omit library and use it with the underscore
 
 export default (state = {} , action ) => {
     switch (action.type) {
@@ -9,6 +10,9 @@ export default (state = {} , action ) => {
             return {...state, [action.payload.id]: action.payload}
         case EDIT_STREAM: 
             return {...state, [action.payload.id]: action.payload}
+        case DELETE_STREAM: 
+            // we use state and get back payload with out the deleted item 
+            return _.omit(state, action.payload.id)
         default:
             return state
     }
