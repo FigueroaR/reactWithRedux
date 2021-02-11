89c34acb50c4c23428ab427741c5fc47e50a1601
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-11 13:42:20 -0500

    mergin a list of Records

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,11 +1,12 @@
 import {CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, EDIT_STREAM, DELETE_STREAM } from '../actions/types'
-import _ from 'loadash'
-//we import the omit library and use it with the underscore
+import _ from 'lodash'
+//we import the omit function and use it with the underscore
 
 export default (state = {} , action ) => {
     switch (action.type) {
         case FETCH_STREAM:
-            return {...state, [action.payload.id]: action.payload}
+            // we are mappin this array into objects
+            return {...state, ..._.mapKeys(action.payload, 'id')}
         case CREATE_STREAM:
             return {...state, [action.payload.id]: action.payload}
         case EDIT_STREAM: 
