699ed09af28b4e4dc9f4a986a842d00fd751ee80
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-11 14:12:11 -0500

    rendering all streams

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -9,10 +9,34 @@ class StreamList extends React.Component {
         //console.log('component did mount')
     }
 
+    renderList() {
+        return this.props.streams.map( stream => {
+            return(
+                <div className="item" key={stream.id}>
+                    <i className="large middle aliged icon camera"/>
+                    <div className="content">{stream.title}
+                        <div className="description" >{stream.description}</div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
-        return <div>Streams List</div>
+       
+        return(
+            <div>
+                <h2>Streams</h2>
+                <div className=" ui celled list">{this.renderList()}</div>
+            </div>
+        )
     }
 }
 
+const mapStateToprops = (state) => {
+    /// Object values is a built in s function, takes a object as
+    // an argument
+    return {streams: Object.values(state.streams) }
+}
 
-export default connect(null, {fetchStreams})(StreamList);
+export default connect(mapStateToprops, {fetchStreams})(StreamList);
