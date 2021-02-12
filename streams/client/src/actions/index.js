a2561ba7f575b2a1375024861544f0416af2a6d5
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-12 12:59:21 -0500

    Edit Form Submission

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -41,7 +41,7 @@ export const editStream = (id, formValues) => async dispatch => {
     // put request with axios
     const response = await streams.put(`/streams/${id}`, formValues)
     dispatch({ type: EDIT_STREAM, payload: response.data})
-
+    history.push('/')
 }
 
 export const deleteStream = (id) => async dispatch => {
